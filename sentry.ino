208d27bb9b744a415bde0300a3b3e81de1e7ee35
chand1012 <chandlerl14@outlook.com>
2019-03-13 09:58:48 -0400

    added arduino file and gitignore

---
+++ sentry.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+#define XPIN 10
+#define TRIGPIN 9
+#define STARTCHAR "<"
+#define ENDCHAR ">"
+
+Servo xservo;
+Servo trigservo;
+
+unsigned int xpos = 0;
+unsigned int spos = 0;
+String command = "";
+
+void setup(){
+    xservo.attach(XPIN);
+    trigservov.attach(TRIGPIN);
+    Serial.begin(9600);
+    while(!Serial){
+        //need serial for operation
+    }
+    Serial.write("Initialized.\n");
+    Serial.write("Starting primary function.\n");
+}
+
+void loop(){
+    while (Serial.available() > 0){
+        command = Serial.readString();
+    }
+}
