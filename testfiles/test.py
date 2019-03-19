c65fff8d4c746e63a15e0592c41202eeb260b4b8
Chandler Lofland <chandlerl14@outlook.com>
2019-03-19 14:47:30 -0400

    changed port

--- testfiles/test.py
+++ testfiles/test.py
@@ -24,4 +24,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port =80, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=1166, debug=True, threaded=True)
